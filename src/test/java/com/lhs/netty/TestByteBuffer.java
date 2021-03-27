b14208e798b5759a7e6a55c4f36179d4d92b1841
longhuashen <huashen361@163.com>
2021-03-27 16:55:05 +0800

    添加注释

--- src/test/java/com/lhs/netty/TestByteBuffer.java
+++ src/test/java/com/lhs/netty/TestByteBuffer.java
@@ -17,20 +17,24 @@ import java.nio.channels.FileChannel;
 public class TestByteBuffer {
 
     public static void main(String[] args) {
-        try (FileChannel fileChannel = new FileInputStream("data.txt").getChannel()){
+        try (FileChannel fileChannel = new FileInputStream("data.txt").getChannel()) {
+            //准备缓冲区
             ByteBuffer buffer = ByteBuffer.allocate(10);
             while (true) {
+                //从channel读取数据，向buffer写入
                 int len = fileChannel.read(buffer);
                 log.debug("读取到的字节数：{}", len);
                 if (len == -1) {
                     break;
                 }
 
+                //切换至读模式
                 buffer.flip();
                 while (buffer.hasRemaining()) {
                     byte b = buffer.get();
-                    log.debug("实际字节：{}", (char)b);
+                    log.debug("实际字节：{}", (char) b);
                 }
+                //切换写模式
                 buffer.clear();
             }
         } catch (IOException e) {
