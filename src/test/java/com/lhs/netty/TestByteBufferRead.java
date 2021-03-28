2b1ab8df4e38b97940d869f40946dd7e9f6b6ab9
huashen <huashen361@gmail.com>
2021-03-29 01:09:59 +0800

    mark&reset 用法

--- src/test/java/com/lhs/netty/TestByteBufferRead.java
+++ src/test/java/com/lhs/netty/TestByteBufferRead.java
@@ -14,6 +14,15 @@ public class TestByteBufferRead {
         ByteBuffer byteBuffer = ByteBuffer.allocate(10);
         byteBuffer.put(new byte[]{'a', 'b', 'c', 'd'});
         byteBuffer.flip();
-        byteBuffer.get(new byte[4]);
+
+        //mark & reset
+        System.out.println((char)byteBuffer.get());
+        System.out.println((char)byteBuffer.get());
+        byteBuffer.mark();
+        System.out.println((char)byteBuffer.get());
+        System.out.println((char)byteBuffer.get());
+        byteBuffer.reset();
+        System.out.println((char)byteBuffer.get());
+        System.out.println((char)byteBuffer.get());
     }
 }
