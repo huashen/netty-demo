77db9fa584f26f5b023f4f17dfe2340667f83e2c
huashen <huashen361@gmail.com>
2021-03-30 02:39:11 +0800

    集中写入

---
+++ src/test/java/com/lhs/netty/TestGatheringWrites.java
@@ -0,0 +1,29 @@
+package com.lhs.netty;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * TestGatheringWrites
+ *
+ * 集中写入
+ *
+ * @author longhuashen
+ * @since 2021-03-30
+ */
+public class TestGatheringWrites {
+
+    public static void main(String[] args) {
+        ByteBuffer b1 = StandardCharsets.UTF_8.encode("hello");
+        ByteBuffer b2 = StandardCharsets.UTF_8.encode("world");
+        ByteBuffer b3 = StandardCharsets.UTF_8.encode("你好");
+
+        try (FileChannel fileChannel = new RandomAccessFile("words2.txt", "rw").getChannel()) {
+            fileChannel.write(new ByteBuffer[]{b1, b2, b3});
+        } catch (IOException e) {
+        }
+    }
+}
