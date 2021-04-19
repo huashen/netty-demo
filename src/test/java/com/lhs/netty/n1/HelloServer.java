6839d35c913a48e05983ee7b306979259de6c7a3
huashen <huashen361@gmail.com>
2021-04-19 23:23:47 +0800

    53-netty helloworld

---
+++ src/test/java/com/lhs/netty/n1/HelloServer.java
@@ -0,0 +1,39 @@
+package com.lhs.netty.n1;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * HelloServer
+ *
+ * @author longhuashen
+ * @since 2021-04-19
+ */
+public class HelloServer {
+
+    public static void main(String[] args) {
+        new ServerBootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new StringDecoder());
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                //打印转换后的字符串
+                                System.out.println(msg);
+                            }
+                        });
+                    }
+                }).bind(8081);
+    }
+}
