740eba38a06142c82faf79a6eb2c4e25ebeecc16
huashen <huashen361@gmail.com>
2021-04-20 22:12:54 +0800

    54-hello world-client

---
+++ src/test/java/com/lhs/netty/n1/HelloClient.java
@@ -0,0 +1,34 @@
+package com.lhs.netty.n1;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+
+/**
+ * HelloClient
+ *
+ * @author longhuashen
+ * @since 2021-04-20
+ */
+public class HelloClient {
+
+    public static void main(String[] args) throws InterruptedException {
+        new Bootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        nioSocketChannel.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                .connect(new InetSocketAddress("localhost", 8081))
+                .sync()
+                .channel()
+                .writeAndFlush("hello,world");
+    }
+}
