201672818022c01e9ed97dd0904ba7f8595d6b3f
huashen <huashen361@gmail.com>
2021-04-24 12:28:52 +0800

    67-channelFuture处理关闭

---
+++ src/test/java/com/lhs/netty/n1/CloseFutureClient.java
@@ -0,0 +1,63 @@
+package com.lhs.netty.n1;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.InetSocketAddress;
+import java.util.Scanner;
+
+/**
+ * CloseFutureClient
+ *
+ * @author longhuashen
+ * @since 2021-04-24
+ */
+@Slf4j
+public class CloseFutureClient {
+
+    public static void main(String[] args) throws InterruptedException {
+        ChannelFuture channelFuture = new Bootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        nioSocketChannel.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                //异步非阻塞 main发起了调用，真正执行 connect是nio线程
+                .connect(new InetSocketAddress("localhost", 8081));
+        Channel channel = channelFuture.sync().channel();
+        log.debug("{}", channel);
+        new Thread(() -> {
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                String line = scanner.nextLine();
+                if ("q".equals(line)) {
+                    channel.close();
+                    break;
+                }
+                channel.writeAndFlush(line);
+            }
+        }, "input").start();
+
+        ChannelFuture closeFuture = channel.closeFuture();
+        closeFuture.sync();
+        log.debug("处理关闭之后的操作");
+
+        //第二种方法
+//        closeFuture.addListener(new ChannelFutureListener() {
+//            @Override
+//            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+//                log.debug("处理关闭之后的操作");
+//            }
+//        });
+    }
+}
