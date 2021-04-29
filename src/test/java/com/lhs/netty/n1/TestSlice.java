6df2d05699e1b77f6b3a92cc1ad2b5c6062a92e7
huashen <huashen361@gmail.com>
2021-04-29 22:47:01 +0800

    slice:stage 2

--- src/test/java/com/lhs/netty/n1/TestSlice.java
+++ src/test/java/com/lhs/netty/n1/TestSlice.java
@@ -16,5 +16,17 @@ public class TestSlice {
         ByteBuf buf = ByteBufAllocator.DEFAULT.buffer(10);
         buf.writeBytes(new byte[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'});
         LogUtil.log(buf);
+
+        //数据没有发生复制 效率高
+        ByteBuf buf1 = buf.slice(0, 5);
+        ByteBuf buf2 = buf.slice(5, 5);
+        LogUtil.log(buf1);
+        LogUtil.log(buf2);
+
+        System.out.println("-----");
+        buf1.setByte(0, 'b');
+        LogUtil.log(buf1);
+        LogUtil.log(buf);
+
     }
 }
